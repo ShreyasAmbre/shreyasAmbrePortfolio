88516349f9b64ee9929088e9c3fa2486856f1f83
ShreyasAmbre <shreyasambre00@gmail.com>
2024-04-22 16:28:24 +0530

    Mail feature integrated

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -30,51 +30,19 @@ export class ContactComponent implements OnInit {
 
 
   sendMail(formData:any){
-    this.emailSendSuccess = undefined
-    console.log("CONTACT FORM DETAILS", formData);
-    
-    // ELACTICMAIL Services :- 
-    // API KEY of  shreyasAmbreEmailSendAPI:- 
-    // B449045B07BB7F598D7FEAFEF7EE414E8C519921A92D2E81109BF07AAE6B616D6E22B8227C53B20E0644FC52D56B5B54
-
-
     let reqObj = {
-      apikey:"B449045B07BB7F598D7FEAFEF7EE414E8C519921A92D2E81109BF07AAE6B616D6E22B8227C53B20E0644FC52D56B5B54",
-      subject:"Test SUbject",
-      from:"abc@gmail.com",
-      to:"shreyasambre00@gmail.com",
-      body: "Email Test Body",
-      isTransactional: true,
-      transactionID: 'shreyasAmbreEmailSendAPI',
+      "to": "shreyasambre00@gmail.com",
+      "subject": formData.senderSubject,
+      "text": formData.senderMsg
     }
-
-    // this.httpc.post('http://api.elasticemail.com/v2/email/send?apikey=B449045B07BB7F598D7FEAFEF7EE414E8C519921A92D2E81109BF07AAE6B616D6E22B8227C53B20E0644FC52D56B5B54&subject=TEST_SUBJECT&from=shreyasambre00@gmail.com&fromName=ABC&sender=shreyasambre00@gmail.com&senderName=SHREYASAMBRE&msgFrom=&msgFromName=&replyTo=&replyToName=&to=&msgTo=', reqObj).subscribe(res => {
-    //   console.log("SMTP RES ===>", res)
-    // })
-
-    this.httpc.post(`https://api.elasticemail.com/v2/email/send?apikey=B449045B07BB7F598D7FEAFEF7EE414E8C519921A92D2E81109BF07AAE6B616D6E22B8227C53B20E0644FC52D56B5B54
-    &subject=${formData.senderSubject}
-    &from=shreyasambre00@gmail.com
-    &fromName=SHREYAS AMBRE
-
-    &sender=${formData.senderEmail}
-    &senderName=${formData.senderName}
-
-    &msgFrom=shreyasambre00@gmail.com
-    &msgFromName=SHREYAS AMBRE
-    
-    &replyTo=${formData.senderEmail}
-    &replyToName=${formData.senderName}
-    &to=${formData.senderName}
-    &msgTo=&msgCC=&msgBcc=&lists=&segments=&mergeSourceFilename=&dataSource=&channel=&bodyHtml=&bodyText=&charset=&charsetBodyHtml=&charsetBodyText=`, reqObj).subscribe((res:any) => {
-      console.log("SMTP RES ===>", res)
-
-      if(res.success === true){
-        this.emailSendSuccess = true
-      }else{
-        this.emailSendSuccess = false
-      }
+    console.log("MAIL SENT ===>", reqObj)
+    this.httpc.post('https://wax-boiling-creature.glitch.me/send-mail', reqObj).subscribe((res:any) => {
+      console.log("MAIL SENT", res)
+    }, (error:any) => {
+      console.log("MAIL SENT", error)
     })
+
+
 
 
 
